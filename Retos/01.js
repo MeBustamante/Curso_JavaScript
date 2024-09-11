be52e42d53b1b7d3da619ac6fcdaa414c2ef063f
MeBustamante <marcelobustamantevillarroel@gmail.com>
2024-09-11 19:36:39 -0300

    subiendo contador de retos

---
+++ Retos/01.js
@@ -0,0 +1,10 @@
+let texto = prompt("ingresa un de texto").toLowerCase()
+let contadorA = 0;
+let contadorE = 0;
+let contadorI = 0;
+let contadorO = 0;
+let contadorU = 0;
+
+for (let i = 0; i < texto.length; i++) {
+    console.log(i);
+}

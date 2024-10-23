4effd4c76625a5c51395b53630c600356c203eef
MeBustamante <marcelobustamantevillarroel@gmail.com>
2024-10-23 19:05:22 -0300

    subiendo ejercicio Gradiente

--- Curso_ReactNative/my-apli/App.js
+++ Curso_ReactNative/my-apli/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient'; // Importa el LinearGradient
+import { LinearGradient } from 'expo-linear-gradient'; // importa el LinearGradient
 
 export default function App() {
   return (
     <LinearGradient 
-      colors={['#833ab4', '#fd1d1d', '#fcb045']} // Cambié el color
-      style={styles.container} // Aplica el estilo container para el tamaño
+      colors={['#833ab4', '#fd1d1d', '#fcb045']} // los codigos de colores, se buscan en google
+      style={styles.container} // aplica el estilo container para el tamaño
     >
       <Text style={styles.text}>Hola mundo, con una imagen cálida de fondo.</Text>
       <StatusBar style="auto" />
@@ -23,6 +23,6 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20, // tamaño del texto
     fontWeight: '600', // negrita
-    color: 'black', // verde
+    color: 'black', // letra color negro
   },
 });
